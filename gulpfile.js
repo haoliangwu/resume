0bf492306f738a78233e63427a813b0e4fb057ab
Lyon Wu <haoliang.wu@liferay.com>
2016-08-12 15:08:54 +0800

    SF

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ const gulp = require('gulp')
 const ghPages = require('gulp-gh-pages')
 
 gulp.task('deploy', function () {
-  return gulp.src('./dist/**/*')
+  return gulp.src('./build/**/*')
     .pipe(ghPages())
 })
